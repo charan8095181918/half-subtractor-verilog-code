abe49fd3243ce8dd90260de4164d5d3ed4283064
charan8095181918 <60030130+charan8095181918@users.noreply.github.com>
2022-04-04 13:14:52 +0530

    Add files via upload

---
+++ halfsub.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:02:37 04/04/2022 
+// Design Name: 
+// Module Name:    halfsub 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module halfsub
+(
+   input wire a,b,
+   output wire diff,borrow
+);
+
+assign diff=(a&~b)|(~a&b);
+assign borrow=~a&b;
+endmodule
